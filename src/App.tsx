6a72483afdfda297feb6dbcbfd2c2f489610f110
frkngrpnr <gurpinarfurkan@gmail.com>
2026-08-02 06:17:10 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello, React + GitHub Pages 👋</h1>
+      <p>Edit <code>src/App.tsx</code> and save to get started.</p>
+    </div>
+  )
+}
+
+export default App
